ec43b109df1efc7b1cfef0250e0f9c5452228ad0
Gabriel <gardengab@outlook.com>
2026-08-29 17:02:24 -0300

    new integrant pics added.

--- garden/lib/media.js
+++ garden/lib/media.js
@@ -26,35 +26,40 @@ export const posterFiles = {
 }
 
 export const memberPhotos = {
-  milton: '/members/milton-vocalista.jpg',
-  gabriel: '/members/gabriel-guitarrista.jpg',
-  matheus: '/members/matheus-guitarrista.jpg',
-  bob: '/members/bob-baixista.jpg',
-  bianca: '/members/bianca-baterista.jpg',
+  milton: '/members/milton-vocalista4.jpg',
+  gabriel: '/members/gabriel-guitarrista4.jpg',
+  matheus: '/members/matheus-guitarrista4.jpg',
+  bob: '/members/bob-baixista4.jpg',
+  bianca: '/members/bianca-baterista4.jpg',
 }
 
 export const memberSlides = {
   milton: [
+    '/members/milton-vocalista4.jpg',
     '/members/milton-vocalista.jpg',
     '/members/milton-vocalista2.jpg',
     '/members/milton-vocalista3.jpg',
   ],
   gabriel: [
+    '/members/gabriel-guitarrista4.jpg',
     '/members/gabriel-guitarrista.jpg',
     '/members/gabriel-guitarrista2.jpg',
     '/members/gabriel-guitarrista3.jpg',
   ],
   matheus: [
+    '/members/matheus-guitarrista4.jpg',
     '/members/matheus-guitarrista.jpg',
     '/members/matheus-guitarrista2.jpg',
     '/members/matheus-guitarrista3.jpg',
   ],
   bob: [
+    '/members/bob-baixista4.jpg',
     '/members/bob-baixista.jpg',
     '/members/bob-baixista2.jpg',
     '/members/bob-baixista3.jpg',
   ],
   bianca: [
+    '/members/bianca-baterista4.jpg',
     '/members/bianca-baterista.jpg',
     '/members/bianca-baterista2.jpg',
     '/members/bianca-baterista3.jpg',
